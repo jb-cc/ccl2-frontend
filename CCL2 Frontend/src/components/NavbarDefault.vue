c27f2905c5231aefd27240d8ebd54bc91f6174a9
Jonas Beer <cc221012@fhstp.ac.at>
2023-06-14 19:41:25 +0200

    Styled navbar a bit

--- CCL2 Frontend/src/components/NavbarDefault.vue
+++ CCL2 Frontend/src/components/NavbarDefault.vue
@@ -1,14 +1,21 @@
 <template>
-  <div class="navbar">
-    <div class="navbar__logo">
-      <router-link to="/">SKINFINITY</router-link>
+  <div class="navbar fixed top-0 z-50 w-full flex items-center justify-between bg-ccl2-Midnight-Blue text-white"
+       style="height: 5.5%;">
+    <div class="flex items-center">
+      <div class="navbar__logo ml-4">
+        <router-link class="text-2xl font-bold font-custom" to="/">SKINFINITY</router-link>
+      </div>
+      <div class="navbar__links ml-4">
+        <router-link class="mx-2 font-custom" to="/listings">MARKET</router-link>
+      </div>
     </div>
-    <div class="navbar__links">
-      <router-link to="/listings">MARKET</router-link>
-      <router-link to="/login">LOGIN</router-link>
-      <router-link to="/register">REGISTER</router-link>
+    <div class="flex items-center mr-4">
+      <router-link class="mx-2 font-custom" to="/login">LOGIN</router-link>
+      <router-link class="mx-2 py-1 px-3 bg-ccl2Purple text-white font-custom" to="/register">REGISTER</router-link>
     </div>
   </div>
+
+
 </template>
 
 <script>
@@ -19,6 +26,3 @@ export default {
 </script>
 
 
-<style scoped>
-/* Any styles specific to this component */
-</style>
