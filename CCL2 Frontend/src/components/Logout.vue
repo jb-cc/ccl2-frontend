253113b707ea5d2a6df7cb21d58f29d08ae8174f
Jonas Beer <cc221012@fhstp.ac.at>
2023-06-19 11:43:52 +0200

    added logout on route /logout

---
+++ CCL2 Frontend/src/components/Logout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <strong>Logging out and redirecting...</strong>
+  </div>
+</template>
+
+<script>
+import http from "../http-common";
+
+export default {
+  name: "Logout",
+  async created() {
+    try {
+      const response = await http.post("http://localhost:8080/logout");
+      if (response.status === 200) {
+        this.$router.replace("/");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
+</script>
