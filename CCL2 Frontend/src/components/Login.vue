d0746bc5256158f33deabfdddd44e7eb7ed86186
Jonas Beer <cc221012@fhstp.ac.at>
2023-06-20 11:43:35 +0200

    styles for login / register

--- CCL2 Frontend/src/components/Login.vue
+++ CCL2 Frontend/src/components/Login.vue
@@ -1,46 +1,53 @@
 <template>
   <div class="flex flex-col items-center justify-center min-h-screen p-6">
-    <div class="w-full max-w-xs">
-      <h1 class="text-2xl font-bold mb-6 text-center text-white">Login</h1>
+    <div class="w-full max-w-sm h-auto bg-white shadow-md">
       <form
-        @submit.prevent="submitForm"
-        class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+          @submit.prevent="submitForm"
+          class="px-8 pt-6 pb-8 mb-4"
       >
+        <div class="flex justify-evenly items-center mb-6 mx-auto">
+          <h1 class="text-xl font-semibold text-center text-black border-b-2 border-black">
+            Login
+          </h1>
+          <h1 class="text-xl font-semibold text-center text-ccl2-Grey-Blue cursor-pointer" @click="navigateToRegister">
+            Register
+          </h1>
+        </div>
         <div class="mb-4">
           <label
-            class="block text-gray-700 text-sm font-bold mb-2"
-            for="username"
+              class="block text-gray-700 text-sm font-semibold mb-2"
+              for="username"
           >
             Username
           </label>
           <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="username"
-            type="text"
-            placeholder="Username"
-            v-model="username"
+              class="shadow shadow-xl border-0 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Username"
+              v-model="username"
           />
         </div>
         <div class="mb-6">
           <label
-            class="block text-gray-700 text-sm font-bold mb-2"
-            for="password"
+              class="block text-gray-700 text-sm font-semibold mb-2"
+              for="password"
           >
             Password
           </label>
           <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-            id="password"
-            type="password"
-            placeholder="Password"
-            v-model="password"
+              class="shadow shadow-xl appearance-none border-0 w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="Password"
+              v-model="password"
           />
         </div>
         <div class="flex items-center justify-between">
           <button
-            @click="submitForm"
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="submit"
+              @click="submitForm"
+              class="w-full bg-ccl2-Light-Gold hover:bg-ccl2-Dark-Gold text-ccl2-Midnight-Blue font-bold py-2 px-4 focus:outline-none focus:shadow-outline"
+              type="submit"
           >
             Log In
           </button>
@@ -49,6 +56,10 @@
     </div>
   </div>
 </template>
+
+
+
+
 
 <script setup>
 import { UserStore } from "@/stores/user";
@@ -66,5 +77,9 @@ const submitForm = async () => {
     console.log("[Login.vue / submitForm] user from Userstore: " + JSON.stringify(user));
     await router.push({ path: `/profile` });
   }
+};
+
+const navigateToRegister = () => {
+  router.push({ path: '/register' });
 };
 </script>
--- CCL2 Frontend/src/components/Register.vue
+++ CCL2 Frontend/src/components/Register.vue
@@ -1,28 +1,69 @@
 <template>
   <div class="flex flex-col items-center justify-center min-h-screen p-6">
-    <div class="w-full max-w-xs">
-      <h1 class="text-2xl font-bold mb-6 text-center text-white">Register</h1>
-      <form @submit.prevent="submitForm" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <div class="w-full max-w-sm h-auto bg-white shadow-md">
+      <form
+          @submit.prevent="submitForm"
+          class="px-8 pt-6 pb-8 mb-4"
+      >
+        <div class="flex justify-evenly items-center mb-6 mx-auto">
+          <h1 class="text-xl font-semibold text-center text-ccl2-Grey-Blue cursor-pointer" @click="navigateToLogin">
+            Login
+          </h1>
+          <h1 class="text-xl font-semibold text-center text-black border-b-2 border-black">
+            Register
+          </h1>
+        </div>
         <div class="mb-4">
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+          <label
+              class="block text-gray-700 text-sm font-semibold mb-2"
+              for="username"
+          >
             Username
           </label>
-          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" v-model="username">
+          <input
+              class="shadow shadow-xl border-0 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Username"
+              v-model="username"
+          />
         </div>
         <div class="mb-4">
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+          <label
+              class="block text-gray-700 text-sm font-semibold mb-2"
+              for="email"
+          >
             Email
           </label>
-          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email" v-model="email">
+          <input
+              class="shadow shadow-xl border-0 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="email"
+              placeholder="Email"
+              v-model="email"
+          />
         </div>
         <div class="mb-6">
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+          <label
+              class="block text-gray-700 text-sm font-semibold mb-2"
+              for="password"
+          >
             Password
           </label>
-          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password" v-model="password">
+          <input
+              class="shadow shadow-xl appearance-none border-0 w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="Password"
+              v-model="password"
+          />
         </div>
         <div class="flex items-center justify-between">
-          <button @click="submitForm" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+          <button
+              @click="submitForm"
+              class="w-full bg-ccl2-Light-Gold hover:bg-ccl2-Dark-Gold text-ccl2-Midnight-Blue font-bold py-2 px-4 focus:outline-none focus:shadow-outline"
+              type="submit"
+          >
             Sign Up
           </button>
         </div>
@@ -30,6 +71,7 @@
     </div>
   </div>
 </template>
+
 
 <script setup>
 import { ref } from 'vue'
@@ -66,4 +108,8 @@ const submitForm = async () => {
     console.log('[Register.vue / submitForm] error response: ' + JSON.stringify(e.response))
   }
 }
+
+const navigateToLogin = () => {
+  router.push({ path: '/login' });
+};
 </script>
